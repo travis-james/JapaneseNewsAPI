fb2cc88d95a9384af813fd1291dd9d87a2473db0
travis <travis.hend@gmail.com>
2021-04-09 12:22:46 -0700

    Accidentally exceeded API limit. Decided to only do trends of twitter.

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/travis-james/RSSReader
 go 1.15
 
 require (
-	cloud.google.com/go v0.81.0
 	github.com/dghubble/go-twitter v0.0.0-20201011215211-4b180d0cc78d
 	github.com/dghubble/oauth1 v0.7.0
-	google.golang.org/api v0.44.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- mytwitter/mytwitter.go
+++ mytwitter/mytwitter.go
@@ -1,12 +1,10 @@
-package mytwitter
+package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"time"
 
-	translate "cloud.google.com/go/translate/apiv3"
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 )
@@ -21,7 +19,6 @@ var (
 	consumerSecret = getenv("TWITTER_CONSUMER_SECRET")
 	accessToken    = getenv("TWITTER_ACCESS_TOKEN")
 	accessSecret   = getenv("TWITTER_ACCESS_TOKEN_SECRET")
-	//wg             = &sync.WaitGroup{}
 )
 
 func main() {
@@ -32,7 +29,7 @@ func main() {
 
 	// twitter client
 	twitcliA := twitter.NewClient(httpClient)
-	trends()
+	trends(twitcliA)
 	//twitcliB := twitter.NewClient(httpClient)
 
 	// Create a google translate client.
@@ -57,87 +54,36 @@ func getenv(name string) string {
 	return v
 }
 
-func retweet(client *twitter.Client, hashtag string) {
-	//defer wg.Done()
+func trends(client *twitter.Client) {
+	// Get current time/date in PST
+	loc, _ := time.LoadLocation("America/Los_Angeles")
+	now := time.Now().In(loc)
+	text := fmt.Sprintf("The current time is: %s\nCurrent trends in Japan are:\n", now)
 
-	// Crack open a stream of tweets.
-	stream, err := client.Streams.Filter(&twitter.StreamFilterParams{
-		Language: []string{"ja"},
-		Track:    []string{hashtag},
-	})
+	// Get a trend list of the latest trends.
+	tl, _, err := client.Trends.Place(jpWOEID, nil)
 	if err != nil {
 		panic(err)
 	}
-	defer stream.Stop()
 
-	for val := range stream.Messages {
-		// Because I don't know what could be coming from the chan interface{}
-		// assert that the value is a tweet.
-		t, ok := val.(*twitter.Tweet)
-		if !ok {
-			fmt.Println("bad val")
-			continue
-		}
-
-		// For some reason t.Retweeted is always false, so my understanding of it
-		// is wrong, but I can't find another parameter where I can check if I've
-		// retweeted, so I'm going by name, I guess.
-		if t.User.ScreenName == me || t.Retweeted {
-			continue
-		}
-
-		_, _, err := client.Statuses.Retweet(t.ID, nil)
-		if err != nil {
-			fmt.Printf("error in retweeting: %d\n %v\n", t.ID, err)
-			continue
-		}
-		fmt.Printf("retweeted: %d\n", t.ID)
+	// Get 5 trends and append them to text.
+	for i := 0; i < 10; i++ {
+		text += "‣ " + tl[0].Trends[i].Name + "\n"
 	}
+	fmt.Println("the text: " + text)
 }
 
-func trends(client *twitter.Client) {
-
-	for {
-		// Get current time/date in PST
-		loc, _ := time.LoadLocation("America/Los_Angeles")
-		now := time.Now().In(loc)
-		text := fmt.Sprintf("The current time is: %s\nCurrent trends in Japan are:\n", now)
-
-		// Get a trend list of the latest trends.
-		tl, _, err := client.Trends.Place(jpWOEID, nil)
-		if err != nil {
-			panic(err)
-		}
-
-		// Get 5 trends and append them to text.
-		for i := 0; i < 5; i++ {
-			tt, err := translateJP(ctx, tl[0].Trends[i].Name, tc)
-			if err != nil {
-				fmt.Printf("Error in translate:\n %v\n", err)
-				continue
-			}
-			text += "‣ " + tt + "\n"
-		}
-		fmt.Println("the text: " + text)
-		_, _, err = client.Statuses.Update(text, nil)
-		if err != nil {
-			fmt.Printf("Error in tweeting trends:\n %v\n", err)
-		}
-		time.Sleep(12 * time.Hour)
-	}
-}
-
-func translateJP(ctx context.Context, text string, tc *translate.TranslationClient) (string, error) {
-	req := &translatepb.TranslateTextRequest{
-		Contents:           []string{text},
-		SourceLanguageCode: "ja",
-		TargetLanguageCode: "en",
-		Parent:             "projects/translation-api-project-307416",
-	}
-	resp, err := tc.TranslateText(ctx, req)
-	if err != nil {
-		fmt.Println("Translate failed.")
-		return "", err
-	}
-	return resp.Translations[0].TranslatedText, nil
-}
+// func translateJP(ctx context.Context, text string, tc *translate.TranslationClient) (string, error) {
+// 	req := &translatepb.TranslateTextRequest{
+// 		Contents:           []string{text},
+// 		SourceLanguageCode: "ja",
+// 		TargetLanguageCode: "en",
+// 		Parent:             "projects/translation-api-project-307416",
+// 	}
+// 	resp, err := tc.TranslateText(ctx, req)
+// 	if err != nil {
+// 		fmt.Println("Translate failed.")
+// 		return "", err
+// 	}
+// 	return resp.Translations[0].TranslatedText, nil
+// }
