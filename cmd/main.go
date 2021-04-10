f2b0f0f735619de61d4c24349bcef0c59b0c0444
98296 <travis.hend@gmail.com>
2021-04-10 15:10:07 -0700

    Variable scoping problem, asked online for help, will come back to this

--- cmd/main.go
+++ cmd/main.go
@@ -3,25 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/travis-james/JapaneseNewsAPI/mytranslate"
 	"github.com/travis-james/JapaneseNewsAPI/mytwitter"
+	"github.com/travis-james/JapaneseNewsAPI/readfeeds"
 )
+
+type News struct {
+	NHK   []readfeeds.NHKItem
+	Asahi []readfeeds.AsahiItem
+	Twit  *mytwitter.TTrends
+	Date  time.Time
+}
 
 // For NHK.
 func main() {
-	// a, err := readfeeds.GetAsahi()
-	// if err != nil {
-	// 	log.Fatalf("%v\n", err)
-	// }
-	// for i, item := range a {
-	// 	a[i].TitleEN, err = mytranslate.TranslateJP(item.Title)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-	// }
-	// for _, item := range a {
-	// 	fmt.Println(item)
-	// }
+	a := News{}
+	var err error
+	a.Asahi, err = readfeeds.GetAsahi()
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+	for i, item := range a {
+		a[i].TitleEN, err = mytranslate.TranslateJP(item.Title)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	for _, item := range a {
+		fmt.Println(item)
+	}
 	// b, err := readfeeds.GetAsahi()
 	// if err != nil {
 	// 	log.Fatalf("%v\n", err)
@@ -33,12 +45,12 @@ func main() {
 	// 	log.Fatalln(err)
 	// }
 	// fmt.Println(d)
-	a, err := mytwitter.GetTrends()
+	c, err := mytwitter.GetTrends()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	for _, trend := range a.Trends {
+	for _, trend := range c.Trends {
 		fmt.Println(trend)
 	}
 
